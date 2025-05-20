0b3c00fa03aba0f68792a27f31b6f8d48a2f3f61
Cauã S. Leal <86691632+ytzamee@users.noreply.github.com>
2025-05-20 13:48:16 -0300

    Update questao3.c

--- exercícios/prova1/questao3.c
+++ exercícios/prova1/questao3.c
@@ -4,34 +4,20 @@
 
 int main(){
 
-	int a, b, c, x, y, z, temp;
+	int a, b, c, x, y, z;
 	int qtd;
 
 	scanf("%d %d %d\n%d %d %d", &a, &b, &c, &x, &y, &z);
 
-	if (a>b && x<y){
-		b=temp;
-		a=b;
-		a=temp;
-	}
-
-	if (x>=a){
+	if (x>=a && y>=b && z>=c){
 		x = x/a;
-	} else {
-		x=0;
-	}
-	if (y>=b){
 		y = y/b;
-	} else {
-		y=0;
-	}
-	if (z>=c){
 		z = z/c;
-	} else {
-		z=0;
-	}
 
-	qtd=x*y*z;
+		qtd=x*y*z;
+	} else {
+		qtd=0;
+	}
 
 	printf("%d\n", qtd);
 
